88d33ed1836f2a976b9a80aa671ffb6feeb61a06
hani abdulle <haniamira24@gmail.com>
2022-08-05 11:13:21 -0600

    incomplete

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-
-function postLogin() {
-
-    let email = document.getElementById("email").value;
-    let pass= document.getElementById("pass").value;
-
-    let login = {
-        email: Hani,
-        password: Inno
-    };
-}
+function loginAttempt(details) {
+    let emailValue = document.getElementById(`email_input`)[`value`];
+    let passwordValue = document.getElementById(`password_input`)[`value`];
+  
+    axios
+      .request({
+        url: `https://reqres.in/api/login`,
+  
+        method: `POST`,
+  
+        data: {
+          email: emailValue,
+          password: passwordValue,
+        },
+      })
+      .then(loginSuccess)
+      .catch(loginFailure);
+  }
+  
+ 
+                                                                                                    
--- home.js
+++ home.js
@@ -1,9 +1,18 @@
-function addColors() {
+let loginSuccess = Cookies.get(`loginToken`);
 
-    let colorsElement = document.getElementById("colors");
+if (loginSuccess) {
+  document.body.insertAdjacentHTML(
+    `afterbegin`,
+    `<h3>Welcome! Your logged in </h3>`
+  );
 
-            
-            
+  axios
+    .request({
+      url: `https://reqres.in/api/unknown`,
+    })
+    .then(loggedInSuccess)
+    .catch(loggedInFailure);
+}
 
      
 
